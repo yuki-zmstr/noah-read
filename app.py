62c784aeab21fbcd34b0bb07c4388cd2602faa4b
yuki-zmstr <yukihide99@gmail.com>
2025-05-17 19:55:07 +0900

    Fix small error in app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from huggingface_hub import InferenceClient
 """
 For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
 """
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+# client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 
 
-def greet(name):
+def greet(name, history):
     return f"Hi {name}, welcome to CapyRead!"
+
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
@@ -18,19 +19,24 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 """
 demo = gr.ChatInterface(
     greet,
-    additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
-    ],
+    examples=["yuki"], title="Echo Bot"
 )
+
+# demo = gr.ChatInterface(
+#     greet,
+#     additional_inputs=[
+#         gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
+#         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
+#         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
+#         gr.Slider(
+#             minimum=0.1,
+#             maximum=1.0,
+#             value=0.95,
+#             step=0.05,
+#             label="Top-p (nucleus sampling)",
+#         ),
+#     ],
+# )
 
 
 if __name__ == "__main__":
